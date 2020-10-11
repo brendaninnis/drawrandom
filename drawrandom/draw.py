667976a9946bed6a93cfc1479385bab77edcc482
Brendan Innis <innisbrendan@gmail.com>
2020-10-10 18:35:34 -0700

    stopped Discord and others from stealing items from the list when pasting the link

--- drawrandom/draw.py
+++ drawrandom/draw.py
@@ -8,7 +8,7 @@ from drawrandom.util import id_generator
 
 bp = Blueprint('draw', __name__, url_prefix='/d')
 
-@bp.route('/<key>')
+@bp.route('/<key>', methods=('GET', 'POST'))
 def draw(key):
     # Fetch an item from the list with key
     item = Item.query.filter(Item.listkey == key).filter(Item.assignee == None).first()
@@ -16,15 +16,18 @@ def draw(key):
     if item is None:
         abort(404, "This list is empty")
 
-    response = make_response(render_template('draw.html', item=item.name))
+    if request.method == 'POST':
+        response = make_response(render_template('draw.html', item=item.name))
 
-    # Give this person a name and assing them to the item
-    username = request.cookies.get('username')
-    if username is None:
-        username = id_generator()
-        response.set_cookie('username', username)
+        # Give this person a name and assing them to the item
+        username = request.cookies.get('username')
+        if username is None:
+            username = id_generator()
+            response.set_cookie('username', username)
 
-    item.assignee = username
-    db.session.commit()
+        item.assignee = username
+        db.session.commit()
+        return response
+    else:
+        return render_template('getdraw.html')
 
-    return response
